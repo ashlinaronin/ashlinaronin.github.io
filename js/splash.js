916d114151d59df9d7501a5057bf573edbb0db15
Ashlin Aronin <ashilnaronin@gmail.com>
2015-11-05 12:12:20 -0800

    Get multi square expansion working

--- js/splash.js
+++ js/splash.js
@@ -3,28 +3,59 @@ $(document).ready(function() {
   var expanded = null;
 
 
-  var expandSquare = function(selector, small, big) {
-    var width, padding;
+  var expandSquare = function(selector, small, big, speed) {
+    // locally scoped width and padding for toggle on the given div
+    var width = small;
+    var padding = small;
     $(selector).click(function(event) {
-      // Toggle width and padding
-      width = (width == big ? small : big);
-      padding = width;
 
-      $(this).animate({
-        width: width,
-        paddingBottom: padding,
-      }, 1000);
+      // Only expand it if there is no other square expanded
+      if (!expanded) {
+        $(this).animate({width: big, paddingBottom: big}, speed);
 
-      // Set expanded to the currently expanded box
-      if (width == big) {
+        // Set currently expanded value and adjust cursors
         expanded = selector;
-        console.log('expanded: ' + expanded);
+        $(this).addClass('nw-resize');
+        $(this).removeClass('se-resize');
+
+      } else if (expanded === selector) {
+        // width = padding = small;
+        $(this).animate({width: small, paddingBottom: small}, speed);
+
+        // Remove currently expanded value and adjust cursors
+        expanded = null;
+        $(this).addClass('se-resize');
+        $(this).removeClass('nw-resize')
+      } else if ((expanded) && (expanded !== selector)) {
+        $(expanded).animate({width: small, paddingBottom: small}, speed);
+        $(expanded).addClass('se-resize');
+        $(expanded).removeClass('nw-resize');
+
+        $(this).animate({width: big, paddingBottom: big}, speed);
+        expanded = selector;
+        $(this).addClass('nw-resize');
+        $(this).removeClass('se-resize');
+        // $(expanded).animate()
+        console.log('trying to open ' + selector + ' but ' + expanded + ' is expanded');
       }
+
+      console.log('in ' + selector + ' after animation, expanded: ' + expanded);
     });
   }
 
-  expandSquare('div#projects', '15%', '50%');
-  expandSquare('div#music', '15%', '50%');
-  expandSquare('div#video', '15%', '50%');
+  var shrink = function(selector) {}
+  var grow = function(selector) {}
+
+
+  // Generate event handlers for 3 divs with given sizes
+  var small = '15%';
+  var big = '40%';
+  var speed = 600;
+  expandSquare('div#projects', small, big, speed);
+  expandSquare('div#music', small, big, speed);
+  expandSquare('div#video', small, big, speed);
 
 });
+
+// Toggle width and padding
+// width = (width == big ? small : big);
