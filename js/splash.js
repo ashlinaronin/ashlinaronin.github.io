42b59f13eaee34a3c89502dc1c104288f8abee18
Ashlin Aronin <ashilnaronin@gmail.com>
2015-11-05 11:25:17 -0800

    Rename js files to reflect new functionality

--- js/splash.js
+++ js/splash.js
@@ -1,19 +1,6 @@
 $(document).ready(function() {
 
-  $('#slideshow > div:gt(0)').hide();
-  setInterval(function() {
-    $('#slideshow > div:first')
-      .fadeOut(1000)
-      .next()
-      .fadeIn(1000)
-      .end()
-      .appendTo('#slideshow');
-  }, 4000);
-
   var expanded = null;
-
-
-
 
 
   var expandSquare = function(selector, small, big) {
