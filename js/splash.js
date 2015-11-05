1dcddb354350ca4d0a8a09550fa50bb6e797452e
Ashlin Aronin <ashilnaronin@gmail.com>
2015-11-05 15:15:29 -0800

    Revert to version before showing content

--- js/splash.js
+++ js/splash.js
@@ -19,15 +19,32 @@ $(document).ready(function() {
   }
 
   var shrink = function(selector) {
+    // var contentChild = '#' + selector.attributes[0].value + '-content';
+    // $(selector).find(contentChild).toggle(speed);
+
     $(selector).animate({width: small, paddingBottom: small}, speed);
     $(selector).addClass('se-resize');
     $(selector).removeClass('nw-resize');
+    // $(selector + '-content').toggle();
+    // console.dir(selector);
+    // var contentChild = '#' + selector.attributes[0].value + '-content';
+    // console.log('contentChild: ' + contentChild);
+    // console.dir(gotit);
   }
 
   var grow = function(selector) {
+    // var contentChild = '#' + selector.attributes[0].value + '-content';
+    // $(selector).find(contentChild).toggle(speed);
+
     $(selector).animate({width: big, paddingBottom: big}, speed);
     $(selector).addClass('nw-resize');
     $(selector).removeClass('se-resize');
+    // $(selector + '-content').toggle();
+    // var contentChild = '#' + selector.attributes[0].value +
+    // var lastchild = $(selector).last();
+
+    // console.dir(lastchild);
+
   }
 
   // Generate click event handlers for 3 divs with given sizes
