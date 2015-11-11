a2a0fa0fbc17184f89bbf950ca76a484df1aa908
Ashlin <ashlinaronin@gmail.com>
2015-11-10 16:36:51 -0800

    Clean up and add comments

--- js/splash.js
+++ js/splash.js
@@ -1,7 +1,6 @@
 $(document).ready(function() {
 
   var expanded = null;
-
   var phasers = [];
 
   var expandSquare = function(selector, small, big, speed) {
@@ -30,9 +29,6 @@ $(document).ready(function() {
 
     var hideText = function() {
       var thisContent = $(selector).find(':nth-child(2)');
-      // var hider = new TweenLite.to(thisContent, )
-      // $(selector).find(':nth-child(2)').hide();
-      // var slowerSpeed = speed/2;
       TweenLite.to(thisContent, speed/2,
         {autoAlpha:0, onComplete: shrinker});
     }
@@ -77,6 +73,22 @@ $(document).ready(function() {
     $(selector).removeClass('se-resize');
   }
 
+
+  var makeBorderPhaser = function(selector, speed, colors) {
+    // Define a new timeline to repeat infinitely back-and-forth.
+    // Make it paused by default so we can play it programmatically.
+    var sqt = new TimelineMax({repeat:-1, yoyo:true, paused:true});
+
+    // Add a tween for each color to the timeline
+    colors.forEach(function(color) {
+      sqt.to(selector, speed, {borderColor:color, ease:easing});
+    });
+
+    // Return this timeline so we can control it from elsewhere
+    return sqt;
+  }
+
+
   // Generate click event handlers for 3 divs with given sizes
   // var small = '15%';
   // var big = '40%';
@@ -88,23 +100,13 @@ $(document).ready(function() {
   expandSquare('div#music', small, big, speed);
   expandSquare('div#video', small, big, speed);
 
-  var makeBorderPhaser = function(selector, speed, colors) {
-    var sqt = new TimelineMax({repeat:-1, yoyo:true, paused:true});
-
-    // add a tween for each color to the timeline
-    colors.forEach(function(color) {
-      sqt.to(selector, speed, {borderColor:color, ease:easing});
-    });
-
-    return sqt;
-  }
-
+  // Set up phasers for borders and save them to scoped array
+  // so we can access them from within the square handlers
   var colors = ['red', 'green', 'blue', 'yellow'];
   var speed = 1;
   var easing = Power3.easeInOut;
   phasers['projects'] = makeBorderPhaser('#projects', speed, colors, easing);
   phasers['music'] = makeBorderPhaser('#music', speed, colors, easing);
   phasers['video'] = makeBorderPhaser('#video', speed, colors, easing);
-
 
 });
